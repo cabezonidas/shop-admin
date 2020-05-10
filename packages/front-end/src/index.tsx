f154840dca0a874aba5f79faf304581fc67aea39
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-11 10:56:01 +1200

    Fix css import

--- packages/front-end/src/index.tsx
+++ packages/front-end/src/index.tsx
@@ -4,7 +4,7 @@ import * as serviceWorker from "./serviceWorker";
 import { GraphqlProvider } from "@cabezonidas/shop-admin-graphql";
 import { UiProvider, useTranslation } from "@cabezonidas/shop-ui";
 import FrontEnd from "./front-end";
-import "@cabezonidas/shop-ui/lib/style.css";
+import "@cabezonidas/shop-ui/assets/style.css";
 
 const uri = (() => {
   switch (process.env.NODE_ENV) {
