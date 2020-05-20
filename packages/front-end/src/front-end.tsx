9a259266a2c7d8874e0b5d51f6bee1f5b91d6b7d
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-20 20:37:25 +1200

    Use layout component instead

--- packages/front-end/src/front-end.tsx
+++ packages/front-end/src/front-end.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import { useTranslation, Box, useTheme, Shell } from "@cabezonidas/shop-ui";
+import { useTranslation, Box, useTheme, ResponsiveLayout } from "@cabezonidas/shop-ui";
 import { MediaApp } from "@cabezonidas/shop-admin-media";
 import { Login, Home, Users, PrivateRoute } from "./components";
 
@@ -28,41 +28,33 @@ export const FrontEnd: React.FC = () => {
   const theme = useTheme();
   return (
     <BrowserRouter basename="/">
-      <Shell>
-        <Box
-          height="100%"
-          display="grid"
-          overflow="hidden"
-          gridTemplateColumns="minmax(200px, auto) 1fr"
-        >
-          <Box bg={theme.colors.primary.dark} color={theme.colors.neutral.lightest} padding="3">
-            <Box display="grid" fontSize="3" gridGap="2" height="max-content">
-              <Box>
-                <Link to="/">{t("main.routes.home")}</Link>
-              </Box>
-              <Box>
-                <Link to="/me">{t("main.routes.me")}</Link>
-              </Box>
-              <Box>
-                <Link to="/users">{t("main.routes.users")}</Link>
-              </Box>
-              <Box>
-                <Link to="/pictures">{t("main.routes.pictures")}</Link>
-              </Box>
+      <ResponsiveLayout
+        nav={
+          <Box display="grid" fontSize="3" gridGap="4" height="max-content" minWidth="200px" p="6">
+            <Box>
+              <Link to="/">{t("main.routes.home")}</Link>
+            </Box>
+            <Box>
+              <Link to="/me">{t("main.routes.me")}</Link>
+            </Box>
+            <Box>
+              <Link to="/users">{t("main.routes.users")}</Link>
+            </Box>
+            <Box>
+              <Link to="/pictures">{t("main.routes.pictures")}</Link>
             </Box>
           </Box>
-          <Box margin="2" overflow="auto" display="flex" flexDirection="column">
-            <Box width="100%" maxWidth="600px" alignSelf="center">
-              <Switch>
-                <Route path="/" exact={true} component={Home} />
-                <Route path="/me" exact={true} component={Login} />
-                <Route path="/pictures" component={MediaApp} />
-                <PrivateRoute path="/users" component={Users} />
-              </Switch>
-            </Box>
-          </Box>
+        }
+      >
+        <Box width="100%" maxWidth="600px" mx="auto" mt="6">
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/me" exact={true} component={Login} />
+            <Route path="/pictures" component={MediaApp} />
+            <PrivateRoute path="/users" component={Users} />
+          </Switch>
         </Box>
-      </Shell>
+      </ResponsiveLayout>
     </BrowserRouter>
   );
 };
