ad687167841373ad733dc718c39f0930aca27300
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-24 18:45:30 +1200

    Render form without title

--- packages/media/src/components/create-album-form.tsx
+++ packages/media/src/components/create-album-form.tsx
@@ -8,46 +8,43 @@ export const CreateAlbumForm = forwardRef<
 >((props, ref) => {
   const { onCreated, ...formProps } = props;
   const { t } = useTranslation();
-  const [create, { data, error, loading }] = useCreateAlbumMutation();
+  const [create, { error, loading }] = useCreateAlbumMutation();
   const { notify } = useToast();
 
   return (
-    <>
-      <Box>{t("media.createAlbum.title")}</Box>
-      <Form
-        onSubmit={async e => {
-          e.preventDefault();
-          if (loading) {
-            return;
-          }
-          const textInput = (e.currentTarget.name as unknown) as HTMLInputElement;
-          const albumName = textInput.value;
-          const { data: res } = await create({ variables: { albumName } });
-          if (res) {
-            onCreated(res.createAlbum);
-            textInput.value = "";
-            notify(t("media.createAlbum.createAlbumSuccess", { album: res.createAlbum }));
-          }
-        }}
-        display="grid"
-        gridGap="2"
-        {...formProps}
-        ref={ref}
+    <Form
+      onSubmit={async e => {
+        e.preventDefault();
+        if (loading) {
+          return;
+        }
+        const textInput = (e.currentTarget.name as unknown) as HTMLInputElement;
+        const albumName = textInput.value;
+        const { data: res } = await create({ variables: { albumName } });
+        if (res) {
+          onCreated(res.createAlbum);
+          textInput.value = "";
+          notify(t("media.createAlbum.createAlbumSuccess", { album: res.createAlbum }));
+        }
+      }}
+      display="grid"
+      gridGap="2"
+      {...formProps}
+      ref={ref}
+    >
+      <Box>
+        <Label htmlFor="name">{t("media.createAlbum.name")}</Label>
+        <Input id="name" disabled={loading} />
+      </Box>
+      <Box>{error && error.graphQLErrors.map((e, i) => <Box key={i}>{e.message}</Box>)}</Box>
+      <Button
+        style={{ cursor: loading ? "wait" : "auto" }}
+        type="submit"
+        variant="primary"
+        ml="auto"
       >
-        <Box>
-          <Label htmlFor="name">{t("media.createAlbum.name")}</Label>
-          <Input id="name" disabled={loading} />
-        </Box>
-        <Box>{error && error.graphQLErrors.map((e, i) => <Box key={i}>{e.message}</Box>)}</Box>
-        <Button
-          style={{ cursor: loading ? "wait" : "auto" }}
-          type="submit"
-          variant="primary"
-          ml="auto"
-        >
-          {t("media.createAlbum.save")}
-        </Button>
-      </Form>
-    </>
+        {t("media.createAlbum.save")}
+      </Button>
+    </Form>
   );
 });
