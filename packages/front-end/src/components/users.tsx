ba3c64080de60fa2b5b44620ac5560c9891e08f3
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-06-21 19:24:18 +1200

    Render users with cards

--- packages/front-end/src/components/users.tsx
+++ packages/front-end/src/components/users.tsx
@@ -1,18 +1,34 @@
 import React, { ComponentProps } from "react";
 import { forwardRef } from "react";
-import { Box, useTranslation } from "@cabezonidas/shop-ui";
-import { useUsersQuery } from "@cabezonidas/shop-admin-graphql";
+import { Box, useTranslation, ProfileCard, H1 } from "@cabezonidas/shop-ui";
+import { useUsersQuery, User } from "@cabezonidas/shop-admin-graphql";
 
 const enUsUsers = {
   users: {
     loading: "Loading users...",
+    users: "User",
   },
 };
 const esArUsers = {
   users: {
     loading: "Cargando usuarios...",
+    users: "Usuarios",
   },
 };
+
+const parseAuthor = (user: User) => ({
+  name: user.name ?? "",
+  dob: user.dob ?? undefined,
+  email: user.email ?? "",
+  imageUrl: user.imageUrl ?? "",
+  linkedin: user.linkedin ?? "",
+  whatsapp: user.whatsapp ?? "",
+  instagram: user.instagram ?? "",
+  facebook: user.facebook ?? "",
+  messenger: user.messenger ?? "",
+  github: user.github ?? "",
+  description: user.description?.map(({ localeId, text }) => ({ localeId, text })) ?? [],
+});
 
 export const Users = forwardRef<HTMLDivElement, ComponentProps<typeof Box>>((props, ref) => {
   const { t, i18n } = useTranslation();
@@ -29,17 +45,18 @@ export const Users = forwardRef<HTMLDivElement, ComponentProps<typeof Box>>((pro
 
   if (data) {
     result = (
-      <>
+      <Box display="grid" gridGap="4">
         {data.users.map(u => (
-          <Box key={u._id}>{u.email}</Box>
+          <ProfileCard border="1px solid" borderRadius={4} key={u._id} author={parseAuthor(u)} />
         ))}
-      </>
+      </Box>
     );
   }
 
   return (
     <Box {...props} ref={ref}>
-      {result}
+      <H1>{t("main.users.users")}</H1>
+      <Box pt="4">{result}</Box>
     </Box>
   );
 });
